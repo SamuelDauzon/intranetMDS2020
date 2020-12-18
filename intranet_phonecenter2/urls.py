a1287d2cc8efa89fa1fcf2975810965c92347f3b
Samuel Dauzon <git@dauzon.com>
2020-12-18 13:11:45 +0100

    Applications : project integration

--- intranet_phonecenter2/urls.py
+++ intranet_phonecenter2/urls.py
@@ -13,9 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+import users
+import customer
+import credits
+import calls
+import supports
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(r'users/', include('users.urls', namespace='users')),
+    path(r'customer/', include('customer.urls', namespace='customer')),
+    path(r'credits/', include('credits.urls', namespace='credits')),
+    path(r'calls/', include('calls.urls', namespace='calls')),
+    path(r'supports/', include('supports.urls', namespace='supports')),
 ]
