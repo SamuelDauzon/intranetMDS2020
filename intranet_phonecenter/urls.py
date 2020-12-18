55ad7e416b355ad858c1309794c5d5cc618e017a
Samuel Dauzon <git@dauzon.com>
2020-12-18 13:41:13 +0100

    Template : Settings paths

--- intranet_phonecenter/urls.py
+++ intranet_phonecenter/urls.py
@@ -1,4 +1,4 @@
-"""intranet_phonecenter2 URL Configuration
+"""intranet_phonecenter URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
--- intranet_phonecenter/wsgi.py
+++ intranet_phonecenter/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for intranet_phonecenter2 project.
+WSGI config for intranet_phonecenter project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'intranet_phonecenter2.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'intranet_phonecenter.settings')
 
 application = get_wsgi_application()
